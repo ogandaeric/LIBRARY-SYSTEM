34b145044332ecf8823d66ba991eebd81ec8ac0b
ogandaeric <98585577+ogandaeric@users.noreply.github.com>
2022-02-23 17:27:15 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+     Project: County Library Management System.
+     Author: ERIC OGANDA
+     Date: Feb,2022
+     Compiler: GNU GCC
+     Language: C99
+     License: MIT
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tCounty Library\n");
+    printf("Welcome Eric Oganda\n");
+    printf("What would you like to do?\n");
+    printf("1. Add New User\n");
+    printf("2. Add Book.\n");
+    printf("Selected Action: ");
+    return 0;
+}
