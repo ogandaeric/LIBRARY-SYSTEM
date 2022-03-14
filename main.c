51819f9fe579352ddac70fa111a01439f3319ab9
ogandaeric <98585577+ogandaeric@users.noreply.github.com>
2022-03-14 11:46:55 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,21 +1,64 @@
-/*
-     Project: County Library Management System.
-     Author: ERIC OGANDA
-     Date: Feb,2022
-     Compiler: GNU GCC
-     Language: C99
-     License: MIT
-*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+struct Patron {
+   char name[100];
+   char pass[30];
+   char email[50];
+   int is_staff;
+};
+
+void add_patron(){
+    struct Patron patron;
+    printf("Enter Name:");
+    gets(patron.name);
+    printf("Enter Email:");
+    gets(patron.email);
+    printf("Initial Password:");
+    gets(patron.pass);
+    printf("Enter 1 if staff 0 otherwise:");
+    scanf("%d",&patron.is_staff);
+    printf("patron %s successfully added\n",patron.name);
+}
+
+int menu()
+{
+    int action;
+    printf("Select as action:\n");
+    printf("1. Add new Patron\n");
+    printf("2. View Patron\n");
+    printf("3. View Books\n");
+    printf("4. Add new Book\n");
+    printf("your action: ");
+    scanf("%d",&action);
+    if(action< 1 || action > 4){
+        printf("Invalid Action. Try Again\n");
+    }
+        return action;
+    }
+    void execute_action(int action){
+    switch(action){
+    case 1:
+        add_patron();
+        break;
+    case 2:
+        printf("Here is a list of patrons\n");
+        break;
+    case 3:
+        printf("Here is a list of all the books");
+        break;
+    case 4:
+        printf("adding a new book\n");
+        break;
+    default:
+        printf("Invalid action.\n");
+    }
+}
 int main()
 {
-    printf("\tCounty Library\n");
-    printf("Welcome Eric Oganda\n");
-    printf("What would you like to do?\n");
-    printf("1. Add New User\n");
-    printf("2. Add Book.\n");
-    printf("Selected Action: ");
+    printf("County Library System");
+    printf("Welcome Mr. Titus!\n");
+    execute_action(menu());
     return 0;
 }
